df10c246efb04b96bc40c1f9cdbd3485d0fcb349
liuaiying <1425577366@qq.com>
2022-07-15 17:54:26 +0800

    layout基本搭建

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,29 @@ Vue.use(VueRouter)
 const routes = [
   // 路由懒加载
   { path: '/login', component: () => import('@/views/Login') },
-  { path: '/', redirect: '/login' }
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      }
+    ]
+  }
 ]
 const router = new VueRouter({
   routes
